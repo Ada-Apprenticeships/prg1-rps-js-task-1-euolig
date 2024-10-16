5221b0830aef402a49a350a0a8657d8646566862
euolig <eunice.ige@ada.ac.uk>
2024-10-16 14:07:06 +0000

    commiting code

--- rps.js
+++ rps.js
@@ -1,19 +1,26 @@
 function rockPaperScissors(player1, player2) {
 
+// defines a dictionary of winning rules, each key represents a choice player1 can make and
+// has a set of corresponding values which it beats
+  const rules = {
+    rock: ["lizard", "scissors"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    spock:["scissors", "rock"],
+    lizard: ["spock","paper"],
+  };
   
-   
-}
-
-
-
-
-
-
-
-
-
+//checks for a draw
+  if (player1 == player2){
+    return "draw"
+  };
+// checks if player2's choice is one of the choices that player1 beats according to the rules. 
+// If yes, it returns "player1", indicating player1 wins. Otherwise, it returns "player2", indicating player2 wins.
+   return rules [player1].includes(player2) ? "player1" : "player2"
+};
 
 // Leave this code here for the automated tests
 module.exports = {
   rockPaperScissors,
 }
+
