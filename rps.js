c8cc102fcf8be9c168d247689ca08ee3f7c025ff
euolig <eunice.ige@ada.ac.uk>
2024-10-16 14:27:37 +0000

    minor fixes

--- rps.js
+++ rps.js
@@ -16,7 +16,7 @@ function rockPaperScissors(player1, player2) {
   };
 // checks if player2's choice is one of the choices that player1 beats according to the rules. 
 // If yes, it returns "player1", indicating player1 wins. Otherwise, it returns "player2", indicating player2 wins.
-   return rules [player1].includes(player2) ? "player1" : "player2"
+   return rules [player1].includes(player2) ? "player1" : "player2";
 };
 
 console.log(rockPaperScissors())
